65f4915fe80d5d29d33c39e78eeaade4fbef8261
Hamed Osama <hamedosama19@gmail.com>
2023-03-07 22:38:13 +0200

    add cookie expire date

--- src/middleware/jwtToken.js
+++ src/middleware/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = async(user, statusCode, res) => {
   // options for cookie
   const options = {
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      Date.now() + process.env.COOKIE_EXPIRE || 90 * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     secure: true,
