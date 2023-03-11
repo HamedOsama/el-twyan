f97443b9b888880b198a93b865588f1785cc2a09
Hamed Osama <hamedosama19@gmail.com>
2023-03-11 20:51:05 +0200

    add search for information

--- src/controller/information.controller.js
+++ src/controller/information.controller.js
@@ -22,8 +22,8 @@ const getInformation = async (req, res, next) => {
     }
     const information = await Information.find({
       $or: [
-        { 'title.en' : req.params.name  },
-        { 'title.ar' : req.params.name  },
+        { 'title.en': req.params.name },
+        { 'title.ar': req.params.name },
       ]
     });
     if (!information) {
@@ -40,7 +40,35 @@ const getInformation = async (req, res, next) => {
   }
 }
 
+const searchInformation = async (req, res, next) => {
+  try {
+    if (!req.params.title) {
+      return next(ServerError.badRequest(401, 'information title is required'));
+    }
+    const information = await Information.find({
+      $or: [
+        { 'title.en': new RegExp(req.params.title, 'i') },
+        { 'title.ar': new RegExp(req.params.title, 'i') },
+
+      ]
+    });
+    if (!information) {
+      return next(ServerError.badRequest(401, 'information not found'))
+    }
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      message: 'succeeded',
+      body: information
+    })
+  } catch (e) {
+    next(e);
+  }
+}
+
+
 module.exports = {
   getAllInformation,
   getInformation,
+  searchInformation
 }
--- src/router/api/information.routes.js
+++ src/router/api/information.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.route('/').get(informationController.getAllInformation);
 router.route('/:name').get(informationController.getInformation);
+router.route('/search/:title').get(informationController.searchInformation);
 
 module.exports = router;
