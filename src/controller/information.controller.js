36422c6acd339cfa14e0535c7f07de1903ec1b80
Hamed Osama <hamedosama19@gmail.com>
2023-03-11 19:59:34 +0200

    update information api

--- src/controller/information.controller.js
+++ src/controller/information.controller.js
@@ -1,4 +1,5 @@
 const Information = require('../model/information');
+const ServerError = require('../utils/ErrorInterface');
 
 const getAllInformation = async (req, res, next) => {
   try {
@@ -19,7 +20,12 @@ const getInformation = async (req, res, next) => {
     if (!req.params.name) {
       return next(ServerError.badRequest(401, 'information name is required'));
     }
-    const information = await Information.findOne({ name: req.params.name });
+    const information = await Information.find({
+      $or: [
+        { 'title.en' : req.params.name  },
+        { 'title.ar' : req.params.name  },
+      ]
+    });
     if (!information) {
       return next(ServerError.badRequest(401, 'information not found'))
     }
